dea1928007ec82e29319a937e0af9a7ad2ac1cd5
AhmedShahbazz <aahmedch420567@gmail.com>
2024-07-11 01:10:08 +0500

    Add project files.

---
+++ WEBSITES/Controllers/CategoryController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WEBSITES.Data;
+using WEBSITES.Models;
+
+namespace WEBSITES.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly DBCONTEXT _db;
+        public CategoryController(DBCONTEXT db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            List<Category> objCategoriesList = _db.Categories.ToList();
+            return View(objCategoriesList);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+
+            _db.Categories.Add(obj);
+            _db.SaveChanges();
+            return RedirectToAction("index");
+        }
+    }
+}
---
+++ WEBSITES/Data/DBCONTEXT.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Xml;
+using WEBSITES.Models;
+
+namespace WEBSITES.Data
+{
+    public class DBCONTEXT : DbContext
+    {
+        public DBCONTEXT(DbContextOptions<DBCONTEXT> options): base(options)
+        {
+
+        }
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name="Action", DisplayOrder=1},
+                new Category { Id = 2, Name = "sci", DisplayOrder = 2 },
+                new Category { Id = 3, Name = "history ", DisplayOrder = 3 }
+
+                );
+        }
+    }
+}
