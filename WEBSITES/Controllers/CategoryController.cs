1cbc24632f6c338b5319d02e1fdfcef5c59a57b3
AhmedShahbazz <aahmedch420567@gmail.com>
2024-07-11 01:10:08 +0500

    Add project files.

---
+++ WEBSITES/Controllers/CategoryController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WEBSITES.Data;
+using WEBSITES.Models;
+
+namespace WEBSITES.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly DBCONTEXT _db;
+        public CategoryController(DBCONTEXT db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            List<Category> objCategoriesList = _db.Categories.ToList();
+            return View(objCategoriesList);
+        }
+    }
+}
